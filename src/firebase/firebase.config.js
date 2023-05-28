21a072221d3f1df7638b3b6e9abbb99d19c50903
f-rahim <freelancerrahim26@gmail.com>
2023-05-28 22:11:48 +0600

    Added firebase & hosting right config

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBHtizFkII8gHwgHgyrWBkCzGLtDzzyBoE",
-  authDomain: "hotel-booking-cf8ef.firebaseapp.com",
-  projectId: "hotel-booking-cf8ef",
-  storageBucket: "hotel-booking-cf8ef.appspot.com",
-  messagingSenderId: "556866470738",
-  appId: "1:556866470738:web:626be30f4b9da53b22308b",
+  apiKey: "AIzaSyC8FdyS2VOg2B2TCJRW-mVqX_pSmxocxU0",
+  authDomain: "hotel-booking-6844c.firebaseapp.com",
+  projectId: "hotel-booking-6844c",
+  storageBucket: "hotel-booking-6844c.appspot.com",
+  messagingSenderId: "954653307680",
+  appId: "1:954653307680:web:11c7f9d46cddb743905d45",
 };
 
 // Initialize Firebase
