73fdc7756c806abf3c6d894a23cdc4d1d922f39b
f-rahim <freelancerrahim26@gmail.com>
2023-05-29 22:41:54 +0600

    Update About page

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,7 +1,14 @@
+import InnerHero from "../../components/InnerHero/InnerHero";
+import Facilities from "../../components/Section/Facilities/Facilities";
 import "./About.css";
 
 const About = () => {
-  return <div className="About-page">About</div>;
+  return (
+    <div className="About-page">
+      <InnerHero>About</InnerHero>
+      <Facilities></Facilities>
+    </div>
+  );
 };
 
 export default About;
