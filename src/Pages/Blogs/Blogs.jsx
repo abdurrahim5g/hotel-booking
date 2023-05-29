6c762cc950aff33f42aec752647cd3a0aa98efeb
f-rahim <freelancerrahim26@gmail.com>
2023-05-29 22:40:43 +0600

    Added InnerHero on Blog Page

--- src/Pages/Blogs/Blogs.jsx
+++ src/Pages/Blogs/Blogs.jsx
@@ -1,7 +1,12 @@
+import InnerHero from "../../components/InnerHero/InnerHero";
 import "./Blogs.css";
 
 const Blogs = () => {
-  return <div className="blogs-page">Blogs</div>;
+  return (
+    <div className="blogs-page">
+      <InnerHero>Blogs</InnerHero>
+    </div>
+  );
 };
 
 export default Blogs;
