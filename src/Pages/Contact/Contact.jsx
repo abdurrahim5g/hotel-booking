3e72b7332de612fd8b1a7dd713ded6f97ca88a77
f-rahim <freelancerrahim26@gmail.com>
2023-05-29 22:44:07 +0600

    Update InnerHero on Contact & Gallery Page

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,7 +1,12 @@
+import InnerHero from "../../components/InnerHero/InnerHero";
 import "./Contact.css";
 
 const Contact = () => {
-  return <div className="Contact-page">Contact</div>;
+  return (
+    <div className="Contact-page">
+      <InnerHero>Contact</InnerHero>
+    </div>
+  );
 };
 
 export default Contact;
--- src/Pages/Gallery/Gallery.jsx
+++ src/Pages/Gallery/Gallery.jsx
@@ -1,7 +1,12 @@
+import InnerHero from "../../components/InnerHero/InnerHero";
 import "./Gallery.css";
 
 const Gallery = () => {
-  return <div className="gallery-page">gallery</div>;
+  return (
+    <div className="gallery-page">
+      <InnerHero>Gallery</InnerHero>
+    </div>
+  );
 };
 
 export default Gallery;
