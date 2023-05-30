b2695646ccf0e7e16c10a252e717df179a53e46b
f-rahim <freelancerrahim26@gmail.com>
2023-05-30 15:36:56 +0600

    Added Error page

--- src/Pages/Error/Error.jsx
+++ src/Pages/Error/Error.jsx
@@ -1,7 +1,32 @@
+import { Link } from "react-router-dom";
 import "./Error.css";
 
 const Error = () => {
-  return <div className="error-page">Error</div>;
+  return (
+    <div className="error-page">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-12">
+            <div className="water-wave text-center ">
+              <h3>4😎4</h3>
+              <h2>You are loss</h2>
+              <div className="go-home">
+                <Link to={"/"}>Go to homepage</Link>
+              </div>
+
+              <div className="waves">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Error;
