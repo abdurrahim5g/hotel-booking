a7f4323dd1297f923446876840eb0e7d643d1674
f-rahim <freelancerrahim26@gmail.com>
2023-05-30 11:59:17 +0600

    Complete Login & Sign Up form Design

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,7 +1,54 @@
+import { Link } from "react-router-dom";
 import "./Login.css";
+import google from "../../assets/images/continue-with-google.png";
 
 const Login = () => {
-  return <div className="login-page">Login</div>;
+  return (
+    <div className="login-page">
+      <div className="login-register-form-container">
+        <div className="form-content">
+          <h3>Login</h3>
+          <form action="#">
+            <div className="single-form-item">
+              <input
+                type="text"
+                name="email"
+                id="email"
+                placeholder="example@gmail.com"
+              />
+            </div>
+
+            <div className="single-form-item">
+              <input
+                type="password"
+                name="password"
+                id="password"
+                placeholder="Password"
+              />
+            </div>
+
+            <div className="single-form-item">
+              <button type="submit">Login</button>
+            </div>
+            <p>
+              New to Ema-john? <Link to={"/sign-up"}>Create New Account</Link>
+            </p>
+            <div className="divider">
+              <span className="line"></span>
+              <span className="or">or</span>
+              <span className="line"></span>
+            </div>
+
+            <div className="continue-with-google">
+              <button>
+                <img src={google} alt="Google Login" />
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -1,7 +1,63 @@
+import { Link } from "react-router-dom";
 import "./SignUp.css";
+import google from "../../assets/images/continue-with-google.png";
 
 const SignUp = () => {
-  return <div className="sign-up-page">Sign up</div>;
+  return (
+    <div className="sign-up-page">
+      <div className="login-register-form-container">
+        <div className="form-content">
+          <h3>Sign Up</h3>
+          <form action="#">
+            <div className="single-form-item">
+              <input
+                type="text"
+                name="name"
+                id="name"
+                placeholder="Enter your name"
+              />
+            </div>
+
+            <div className="single-form-item">
+              <input
+                type="text"
+                name="email"
+                id="email"
+                placeholder="Enter your email"
+              />
+            </div>
+
+            <div className="single-form-item">
+              <input
+                type="password"
+                name="password"
+                id="password"
+                placeholder="Set Password"
+              />
+            </div>
+
+            <div className="single-form-item">
+              <button type="submit">Sign Up</button>
+            </div>
+            <p>
+              Already have an account? <Link to={"/login"}>Login</Link>
+            </p>
+            <div className="divider">
+              <span className="line"></span>
+              <span className="or">or</span>
+              <span className="line"></span>
+            </div>
+
+            <div className="continue-with-google">
+              <button>
+                <img src={google} alt="Google Login" />
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SignUp;
